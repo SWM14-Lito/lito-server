17886e2aadb444aea28614a37dd2026a204ac590
simple <shwncho4@gmail.com>
2023-07-09 15:02:00 +0900

    test: file 예외 케이스 작성

--- src/test/java/com/swm/lito/file/adapter/in/presentation/FileControllerTest.java
+++ src/test/java/com/swm/lito/file/adapter/in/presentation/FileControllerTest.java
@@ -1,5 +1,7 @@
 package com.swm.lito.file.adapter.in.presentation;
 
+import com.swm.lito.common.exception.ApplicationException;
+import com.swm.lito.common.exception.file.FileErrorCode;
 import com.swm.lito.file.application.port.in.FileUseCase;
 import com.swm.lito.support.restdocs.RestDocsSupport;
 import com.swm.lito.support.security.WithMockJwt;
@@ -14,11 +16,16 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.ResultActions;
 
 import static com.swm.lito.support.restdocs.RestDocsConfig.field;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willThrow;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.multipart;
 import static org.springframework.restdocs.request.RequestDocumentation.partWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.requestParts;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(FileController.class)
@@ -53,6 +60,72 @@ class FileControllerTest extends RestDocsSupport {
                         partWithName("file").description("업로드할 파일")
                     )
                 ));
+    }
+
+    @Test
+    @DisplayName("파일 업로드 실패")
+    void file_upload_fail() throws Exception {
+
+        //given
+        MockMultipartFile file = new MockMultipartFile("file", "image.png","image/png","file".getBytes());
+        willThrow(new ApplicationException(FileErrorCode.FILE_UPLOAD_FAIL))
+                .given(fileUseCase).upload(any(),any());
+        //when
+        ResultActions resultActions = mockMvc.perform(
+                multipart("/api/users/files")
+                        .file(file)
+                        .header(HttpHeaders.AUTHORIZATION,"Bearer testAccessToken")
+                        .contentType(MediaType.MULTIPART_FORM_DATA)
+        );
+        //then
+        resultActions
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code",is(FileErrorCode.FILE_UPLOAD_FAIL.getCode())))
+                .andExpect(jsonPath("$.message",is(FileErrorCode.FILE_UPLOAD_FAIL.getMessage())));
+    }
+
+    @Test
+    @DisplayName("파일 업로드 실패 / 파일을 전송하지 않을 경우")
+    void file_upload_fail_not_empty() throws Exception {
+
+        //given
+        MockMultipartFile file = new MockMultipartFile("file", "image.png","image/png","file".getBytes());
+        willThrow(new ApplicationException(FileErrorCode.FILE_EMPTY))
+                .given(fileUseCase).upload(any(),any());
+        //when
+        ResultActions resultActions = mockMvc.perform(
+                multipart("/api/users/files")
+                        .file(file)
+                        .header(HttpHeaders.AUTHORIZATION,"Bearer testAccessToken")
+                        .contentType(MediaType.MULTIPART_FORM_DATA)
+        );
+        //then
+        resultActions
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code",is(FileErrorCode.FILE_EMPTY.getCode())))
+                .andExpect(jsonPath("$.message",is(FileErrorCode.FILE_EMPTY.getMessage())));
+    }
+
+    @Test
+    @DisplayName("파일 업로드 실패 / 파일 용량 초과")
+    void file_upload_fail_exceeded_size() throws Exception {
+
+        //given
+        MockMultipartFile file = new MockMultipartFile("file", "image.png","image/png","file".getBytes());
+        willThrow(new ApplicationException(FileErrorCode.FILE_EXCEEDED_SIZE))
+                .given(fileUseCase).upload(any(),any());
+        //when
+        ResultActions resultActions = mockMvc.perform(
+                multipart("/api/users/files")
+                        .file(file)
+                        .header(HttpHeaders.AUTHORIZATION,"Bearer testAccessToken")
+                        .contentType(MediaType.MULTIPART_FORM_DATA)
+        );
+        //then
+        resultActions
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code",is(FileErrorCode.FILE_EXCEEDED_SIZE.getCode())))
+                .andExpect(jsonPath("$.message",is(FileErrorCode.FILE_EXCEEDED_SIZE.getMessage())));
     }
 
 }
