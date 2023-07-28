6a41f377820476295fbaee397525546a460999a5
simple <shwncho4@gmail.com>
2023-07-28 16:34:12 +0900

    [LITO-126] feat: 스케줄러 구현

---
+++ src/main/java/com/swm/lito/batch/sheduler/RecommendUserJobScheduler.java
@@ -0,0 +1,30 @@
+package com.swm.lito.batch.sheduler;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RecommendUserJobScheduler {
+
+    private final Job job;
+
+    private final JobLauncher jobLauncher;
+
+    public RecommendUserJobScheduler(@Qualifier("recommendUserJob") Job job, JobLauncher jobLauncher) {
+        this.job = job;
+        this.jobLauncher = jobLauncher;
+    }
+
+    @Scheduled(cron = "0 */2 * * * *")
+    public void recommendUserJobRun() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+        jobLauncher.run(job, new JobParameters());
+    }
+}
