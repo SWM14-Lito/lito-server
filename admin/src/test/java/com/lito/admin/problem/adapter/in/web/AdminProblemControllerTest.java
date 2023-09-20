e4e86cef33d8d060f50fc5e774a21fad79a8adce
simple <shwncho4@gmail.com>
2023-09-20 15:16:53 +0900

    test: 관리자 문제 삭제 adapter in 테스트

--- admin/src/test/java/com/lito/admin/problem/adapter/in/web/AdminProblemControllerTest.java
+++ admin/src/test/java/com/lito/admin/problem/adapter/in/web/AdminProblemControllerTest.java
@@ -1,13 +1,18 @@
 package com.lito.admin.problem.adapter.in.web;
 
+import com.lito.admin.config.SecurityConfig;
 import com.lito.admin.problem.adapter.in.request.FaqRequest;
 import com.lito.admin.problem.adapter.in.request.ProblemRequest;
 import com.lito.admin.support.restdocs.RestDocsSupport;
 import com.lito.core.admin.application.port.in.AdminProblemCommandUseCase;
+import com.lito.core.common.exception.ApplicationException;
+import com.lito.core.common.exception.admin.AdminErrorCode;
+import jakarta.servlet.http.Cookie;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
@@ -15,13 +20,19 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.willDoNothing;
+import static org.mockito.BDDMockito.willThrow;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -29,10 +40,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(AdminProblemController.class)
 @MockBean(JpaMetamodelMappingContext.class)
 @WithMockUser(username = "admin", roles = "ADMIN")
+@Import(SecurityConfig.class)
 class AdminProblemControllerTest extends RestDocsSupport {
 
     @MockBean
     private AdminProblemCommandUseCase adminProblemCommandUseCase;
+
+    private final String COOKIE = UUID.randomUUID().toString();
 
     @Test
     @DisplayName("문제 생성 성공")
@@ -56,7 +70,7 @@ class AdminProblemControllerTest extends RestDocsSupport {
         //when
         ResultActions resultActions = mockMvc.perform(
                 post("/api/v1/admin/problems")
-                        .with(csrf())
+                        .cookie(new Cookie("JSESSIONID", COOKIE))
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request))
         );
@@ -89,7 +103,7 @@ class AdminProblemControllerTest extends RestDocsSupport {
         //when
         ResultActions resultActions = mockMvc.perform(
                 post("/api/v1/admin/problems")
-                        .with(csrf())
+                        .cookie(new Cookie("JSESSIONID", COOKIE))
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request))
         );
@@ -98,5 +112,47 @@ class AdminProblemControllerTest extends RestDocsSupport {
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.errors",hasSize(5)));
 
+    }
+
+    @Test
+    @DisplayName("문제 삭제 성공")
+    void delete_problem_success() throws Exception{
+
+        //given
+        Long problemId = 1L;
+        willDoNothing().given(adminProblemCommandUseCase).delete(problemId);
+        //when
+        ResultActions resultActions = mockMvc.perform(
+                delete("/api/v1/admin/problems/{id}",problemId)
+                        .cookie(new Cookie("JSESSIONID", COOKIE))
+        );
+        //then
+        resultActions
+                .andExpect(status().isOk())
+                .andDo(restDocs.document(
+                        pathParameters(
+                                parameterWithName("id").description("문제 id")
+                        )
+                ));
+    }
+
+    @Test
+    @DisplayName("문제 삭제 실패 / 존재하지 않는 문제")
+    void delete_problem_fail_not_found() throws Exception{
+
+        //given
+        Long problemId = 1L;
+        willThrow(new ApplicationException(AdminErrorCode.PROBLEM_NOT_FOUND))
+                .given(adminProblemCommandUseCase).delete(problemId);
+        //when
+        ResultActions resultActions = mockMvc.perform(
+                delete("/api/v1/admin/problems/{id}",problemId)
+                        .cookie(new Cookie("JSESSIONID", COOKIE))
+        );
+        //then
+        resultActions
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code",is(AdminErrorCode.PROBLEM_NOT_FOUND.getCode())))
+                .andExpect(jsonPath("$.message",is(AdminErrorCode.PROBLEM_NOT_FOUND.getMessage())));
     }
 }
